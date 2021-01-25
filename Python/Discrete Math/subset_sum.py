c4cf9c1be8bea956fb01f4cc5ba6b23b6316d940
Bryant Juspi <bjuspi@outlook.com>
2021-01-25 23:56:01 +0800

    brute force subset sum algorithm

---
+++ Python/Discrete Math/subset_sum.py
@@ -0,0 +1,12 @@
+def subset_sum(arr, k):
+    return solve(0, 0, arr, k)
+
+def solve(i, sum, arr, k):
+    if i > len(arr):
+        if sum == k:
+            return True
+        else:
+            return False
+    option1 = solve(i+1, sum+arr[i], arr, k)
+    option2 = solve(i+1, sum, arr, k)
+    return option1 or option2
